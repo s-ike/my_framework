5df29297fcba80c6e79e0a662b0c2ec47d976cea
s-ike <shunt_i@yahoo.co.jp>
2022-02-09 08:41:30 +0900

    update config

--- fw_config/config.php
+++ fw_config/config.php
@@ -3,7 +3,7 @@ define('FW_SITE_TITLE', 'サンプルページ');
 define('FW_TOP_URL', '');
 
 // サーバー環境
-if ($_SERVER['SERVER_ADDR'] === '202.181.101.78') {
+if ($_SERVER['SERVER_ADDR'] === '200.100.100.100') {
     define('BLOG_SITE_SERVER_ENV', 'production');
 } elseif ($_SERVER['SERVER_ADDR'] === '192.168.33.50') {
     define('BLOG_SITE_SERVER_ENV', 'dev');
